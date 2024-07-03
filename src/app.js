b109543f21db21aacf641f196719fe18915e9a4d
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-07-03 09:42:37 -0300

    base de datos ok

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@
 import express from "express"
 import {engine} from "express-handlebars"
 import "./database.js"
-
-
+import displayRoutes from 'express-routemap'
+import imagenRouter from "./routes/imagen.router.js"
+import multer from "multer"
 
 const app = express()
 const PORT = 8080
@@ -16,16 +17,30 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static("./src/public"))
 
+//Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cd) =>{
+        cd(null, "./src/public/img")
+    },
+    filename: (req, file, cd) =>{
+        cd(null, file.originalname)
+    }
+})
+app.use(multer({storage}).single("image"))
+
+
 //Handlebars
 app.engine("handlebars", engine())
 app.set("view engine", "handlebars")
 app.set("views", "./src/views")
 
 //Rutas
-app.get("/", (req, res) =>{
-    res.render("index")
-})
+app.use('/', imagenRouter)
 
+
+//Listen
 app.listen(PORT, ()=>{
     console.log(`# # # Puerto ${PORT} ONLINE # # #`)
+    displayRoutes(app)
+    
 })
--- src/routes/imagen.router.js
+++ src/routes/imagen.router.js
@@ -1,21 +1,46 @@
 import { Router} from "express"
+import {promises as fs} from "fs"
 import ImagenModel from "../models/imagen.model.js"
-
 const router = Router()
 
 
 
 //ruta raiz de la app
 router.get("/", async (req, res)=>{
-    const imagenes = await ImagenModel.find()
-    
-    
-    
+    const imagenes = await ImagenModel.find().lean()
+    //con lean() es una forma de evitar la medida de seguridad de handlebars que daba error
+    // la otra es mapearlo
     res.render("index", {imagenes: imagenes})
 })
 
+//ruta para acceder al formulario de carga
+router.get("/upload", (req, res) => {
+    res.render("upload")
+})
 
+router.post("/upload", async (req, res) =>{
+    try {
+        const imagen = new ImagenModel()
+        imagen.title = req.body.title
+        imagen.description = req.body.description
+        imagen.filename = req.body.filename
+        imagen.path = "/img/" + req.file.filename
+        
+        //Guardamos el objeto en la base de datos
+        await imagen.save()
+        res.redirect("/")
 
+    } catch(error) {
+        res.status(500).send("Error en el servidor")
+    }
+})
 
+//Ruta para eliminar imagenes
+router.get("/image/:id/delete", async (req,res) =>{
+    const {id} = req.params
+    const imagen = await ImagenModel.findByIdAndDelete(id)
+    await fs.unlink("./src/public" + imagen.path)
+    res.redirect("/")
+})
 
 export default router
